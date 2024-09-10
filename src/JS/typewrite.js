6d753fff48f4af8ca3019aa0053b89de55cf1568
statikana <86629697+statikana@users.noreply.github.com>
2024-09-10 08:33:35 -0400

    fix

--- src/JS/typewrite.js
+++ src/JS/typewrite.js
@@ -1,17 +1,11 @@
 var nameText = document.getElementById('nameTypewrite');
 
-var writeDescriptionText = new Typewriter(descriptionText, {
-    loop: false,
-    delay: 85
-});
-
 var writeNameText = new Typewriter(nameText, {
-    loop: false,
-    delay: 100
+    loop: false
 });
 
 function runWithDelay() {
-    writeNameText.typeString("my name is statikana")
+    writeNameText.typeString("im statikana")
         .callFunction(() => {document.querySelector(".Typewriter__cursor").remove();})
         .start();
 }
