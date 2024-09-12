0148e8c93dd8ab2a0731351ba84709089c7be8b6
statikana <86629697+statikana@users.noreply.github.com>
2024-09-12 12:18:11 -0400

    progress

--- src/JS/snake.js
+++ src/JS/snake.js
@@ -1,9 +1,58 @@
-window.onload = function () {
-	document.addEventListener("keyup", loadKey);
+let width = 10; // boxes
+let height = 10; // boxes
+let size = 10; // px
+let squares = [];
+
+let snake = [2, 1, 0]; // head is front, tail is back
+
+let direction = "";
+document.addEventListener("keydown", loadKey);
+
+window.onload = createBoard;
+
+function frame() {
+	move();
+	appleCheck();
+	checkForCollisions();
+}
+
+function al() {
+	alert("ok");
+}
+
+function createBoard() {
+	let grid = document.querySelector(".grid");
+	for (let i = 0; i < 100; i++) {
+		var new_div = document.createElement("div");
+		grid.appendChild(new_div);
+		squares.push(new_div);
+	};
+}
+
+function move() {
+	let head_index = snake[0];
+	let new_head_index = 0;
+	if (direction == "ArrowUp") {
+		new_head_index = head_index + width;
+	} else if (direction == "ArrowRight") {
+		new_head_index = head_index + 1;
+	} else if (direction == "ArrowDown") {
+		new_head_index = head_index - 10;
+	} else if (direction == "ArrowLeft") {
+		new_head_index = head_index - 1;
+	}
+	snake.push(new_head_index);
+}
+
+function appleCheck() {
+	
 }
 
 function loadKey(key) {
-	part = document.querySelector(".rep");
-	part.textContent = key.code;
-	part.innerHTML = key.code;
+	if (!key.code.startsWith("Arrow")) {
+		alert(">");
+	} else {
+		direction = key.code;
+		document.querySelector(".rep").innerHTML = direction;
+	}
 }
