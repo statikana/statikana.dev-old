8416c1dfc21ea8011db2eb76d8f629d0b62f703b
statikana <86629697+statikana@users.noreply.github.com>
2024-09-13 19:58:31 -0400

    snake done

--- src/JS/snake.js
+++ src/JS/snake.js
@@ -1,14 +1,19 @@
-
 const width = 10;
 const height = 10;
-var squares = []; /* representation of what the canvas should be showing ... s=snake, a=apple, x=blank */
+var squares =
+	[]; /* representation of what the canvas should be showing ... s=snake, a=apple, x=blank */
 
-var snake = [[2, 0], [1, 0], [0, 0]]; // head is front, tail is back
+var snake = [
+	[0, 2],
+	[0, 1],
+	[0, 0],
+]; // head is front, tail is back
+var apple = [];
 
 var canvas = document.querySelector(".snake-canvas");
-var direction = "";
+var direction = "ArrowRight";
 
-document.addEventListener("keydown", loadKey);
+document.addEventListener("keydown", onKey);
 
 window.onload = createBoard;
 
@@ -21,101 +26,166 @@ function coordinatesToIndex(x, y) {
 }
 
 function createBoard() {
+	createSquares();
+	placeRandomApple();
+	draw();
+}
+
+function placeRandomApple() {
+	let apple_coords = snake[0];
+	while (snake.includes(apple_coords)) {
+		apple_coords = indexToCoordinates(
+			Math.floor(Math.random() * width * height)
+		);
+	}
+	apple = apple_coords;
+}
+
+
+function onKey(key) {
+	/* trigged on key press, activates the movement mechanics */
+	if (key.code === "Space") {
+		location.reload();
+	} else if (equalsInverseDirection(key.code)) {
+		return;
+	} else {
+		direction = key.code;
+		frame();
+	}
+}
+
+function equalsInverseDirection(code) {
+	/* checks if the current direction is the opposite direction of the given key code (supports arrows and WASD) */
+	switch (code) {
+		case "ArrowUp":
+		case "KeyW":
+			return ["ArrowDown", "KeyS"].includes(direction);
+		case "ArrowRight":
+		case "KeyD":
+			return ["ArrowLeft", "KeyA"].includes(direction);
+		case "ArrowDown":
+		case "KeyS":
+			return ["ArrowUp", "KeyW"].includes(direction);
+		case "ArrowLeft":
+		case "KeyA":
+			return ["ArrowRight", "KeyD"].includes(direction);
+		default:
+			return null;
+	}
+}
+
+function frame() {
+	/* updates the frame in the game */
+	console.log("frame");
+	calculateHead();
+	checkForSelfCollision();
+	draw();
+}
+
+function checkForSelfCollision() {
+	let head = snake[0];
+	snake.slice(1).forEach(pos => {
+		if (pos[0] == head[0] && pos[1] == head[1]) {
+			alert("You ran into yourself! Press space to try again.");
+		}
+	});
+}
+
+function calculateHead() {
+	/* moves the head in the indicated arrow direction */
+	let head_coords = snake[0];
+	let new_coords = [-1, -1];
+	switch (direction) {
+		case "ArrowUp":
+		case "KeyW":
+			new_coords = [head_coords[0] - 1, head_coords[1]];
+			break;
+		case "ArrowRight":
+		case "KeyD":
+			new_coords = [head_coords[0], head_coords[1] + 1];
+			break;
+		case "ArrowDown":
+		case "KeyS":
+			new_coords = [head_coords[0] + 1, head_coords[1]];
+			break;
+		case "ArrowLeft":
+		case "KeyA":
+			new_coords = [head_coords[0], head_coords[1] - 1];
+			break;
+		default:
+			return false;
+	}
+	let head = new_coords;
+	if (head[0] < 0) {
+		head[0] = width - 1;
+	}
+	if (head[0] >= width) {
+		head[0] = 0;
+	}
+	if (head[1] < 0) {
+		head[1] = height - 1;
+	}
+	if (head[1] >= width) {
+		head[1] = 0;
+	}
+	snake.unshift(head);
+
+	if (apple[0] != head[0] || apple[1] != head[1]) {
+		snake.pop();
+		console.log("pop ^")
+	} else {
+		placeRandomApple();
+	}
+	return true;
+}
+
+function createSquares() {
+	/* sets `squares` to an empty array of width x height "x" */
+	squares = [];
 	for (let i = 0; i < width; i++) {
 		squares.push([]);
 		for (let j = 0; j < height; j++) {
 			squares[i].push("x");
 		}
 	}
-	snake.forEach(pair => {
-		squares[pair[0]][pair[1]] = "s";
+}
+
+function setSquares() {
+	/* notes `squares` to the existing entities */
+	snake.forEach((pos) => {
+		squares[pos[0]][pos[1]] = "s";
 	});
-	placeRandomApple();
-	draw();
-}
+	squares[snake[0][0]][snake[0][1]] = "h";
 
-function placeRandomApple() {
-	console.log(snake[0]);
-	let apple_coords = snake[0];
-	while (snake.includes(apple_coords)) {
-		apple_coords = indexToCoordinates(Math.floor(Math.random() * width * height))
-		console.log(apple_coords);
-	}
-	squares[apple_coords[0]][apple_coords[1]] = "a";
-
-}
-
-function loadKey(key) {
-	/* checks for input and sets it to direction */
-	if (!key.code.startsWith("Arrow")) {
-		null;
-	} else {
-		direction = key.code;
-		document.querySelector(".rep").innerHTML = direction;
-		
-	}
-}
-
-function frame() {
-	move();
-	appleCheck();
-	checkForCollisions();
-	draw();
-}
-
-
-function move() {
-	/* moves the head in the indicated arrow direction */
-	let head_index = snake[0];
-	let new_head_index = 0;
-	if (direction == "ArrowUp") {
-		new_head_index = head_index + width;
-	} else if (direction == "ArrowRight") {
-		new_head_index = head_index + 1;
-	} else if (direction == "ArrowDown") {
-		new_head_index = head_index - width;
-	} else if (direction == "ArrowLeft") {
-		new_head_index = head_index - 1;
-	}
-	snake.push(new_head_index);
-}
-
-function appleCheck() {
-	/* cuts the the tail of the snake if it doesnt get an apple */
-	if (squares[snake[0]].classList.contains("apple")) {
-		squares[snake[0]].classList.remove("apple");
-	} else {
-		snake.pop();
-	}
-}
-
-function checkForCollisions() {
-	let head = snake[0];
-	let coords = indexToCoordinates(head);
-	if (
-		!(0 <= coords[0] < width) ||
-		!(0 <= coords[1] < height)
-	) {
-		alert("u died L, reload page or sumn");
-	}
+	squares[apple[0]][apple[1]] = "a";
 }
 
 function draw() {
+	/* draws `squares` to the canvass */
 	console.log("draw");
+	createSquares();
+	setSquares();
+
 	let ctx = canvas.getContext("2d");
-	for (let i = 0; i < 800; i+=80) {
-		for (let j = 0; j < 800; j+=80) {
-			item = squares[i/80][j/80];
-			let fill = ""
+	for (let i = 0; i < 800; i += 80) {
+		for (let j = 0; j < 800; j += 80) {
+			item = squares[j / 80][i / 80];
+			let fill = "";
 			switch (item) {
 				case "x":
-					fill = "#222";
+					fill = "#222222";
 					break;
 				case "s":
 					fill = "#00ff00";
 					break;
+				case "h":
+					fill = "#00dd00";
+					break;
 				case "a":
 					fill = "#ff0000";
+					break;
+				default:
+					fill = "#ffffff";
 					break;
 			}
 			ctx.fillStyle = fill;
